a0bbbf215675ab62278b7775f3fc2b023c6eec59
komawei <komawei@126.com>
2019-03-01 09:36:24 +0800

    initial commit

---
+++ wechat-coupon-api/src/main/java/cn/eeka/sharedcenter/wechat/WechatCouponApiApplication.java
@@ -0,0 +1,13 @@
+package cn.eeka.sharedcenter.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatCouponApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatCouponApiApplication.class, args);
+	}
+
+}
