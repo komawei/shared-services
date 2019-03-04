78648b1365b74ce45939827713665ef999afcce9
e <komawei@126.com>
2019-03-04 22:59:25 +0800

    add eureka client module

--- wechat-coupon-api/src/main/java/cn/eeka/sharedservices/WechatCouponApplication.java
+++ wechat-coupon-api/src/main/java/cn/eeka/sharedservices/WechatCouponApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @EnableTransactionManagement
 @SpringBootApplication
+@EnableEurekaClient
 public class WechatCouponApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
