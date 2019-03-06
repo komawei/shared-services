0b2778d0e7369c1857e612cfe72e485bdecd3358
komawei <komawei@126.com>
2019-03-06 10:11:04 +0800

    add spring-cloud-ribbon module

---
+++ wechat-coupon-consumer/src/main/java/cn/eeka/sharedservices/WechatCouponConsumerApplication.java
@@ -0,0 +1,25 @@
+package cn.eeka.sharedservices;
+
+/**
+ * @ClassName: WechatCouponConsumerApplication
+ * @Description: TODO
+ * @author: SimonWayne
+ * @date: 2019/3/6 9:43
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: WechatCouponConsumerApplication
+ * @Description: TODO
+ * @author: SimonWayne
+ * @date: 2019/3/4 17:17
+ */
+@SpringBootApplication
+public class WechatCouponConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatCouponConsumerApplication.class, args);
+    }
+}
---
+++ wechat-coupon-consumer/src/main/java/cn/eeka/sharedservices/consumer/controller/WechatConsumerController.java
@@ -0,0 +1,17 @@
+package cn.eeka.sharedservices.consumer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: WechatConsumerController
+ * @Description: TODO
+ * @author: SimonWayne
+ * @date: 2019/3/6 9:45
+ */
+@RestController
+public class WechatConsumerController {
+
+    public String listAll() {
+        return "1234";
+    }
+}
