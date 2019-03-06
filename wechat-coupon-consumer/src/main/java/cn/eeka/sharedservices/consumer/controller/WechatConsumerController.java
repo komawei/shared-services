7698621a23ea9b913430a40b6cd1ef8852b3b8eb
komawei <komawei@126.com>
2019-03-06 14:50:34 +0800

    add spring-cloud-feign module

--- wechat-coupon-consumer/src/main/java/cn/eeka/sharedservices/consumer/controller/WechatConsumerController.java
+++ wechat-coupon-consumer/src/main/java/cn/eeka/sharedservices/consumer/controller/WechatConsumerController.java
@@ -5,9 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @ClassName: WechatConsumerController
  * @Description: TODO
